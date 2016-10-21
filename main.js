9c5432578c8d852cc8b0ddf4e8b3bfff93a059a0
Paul Fischer <paul.a.fischer@intel.com>
2016-10-20 17:24:31 -0700

    Major update. See detailed description.
    - renamed <project-name>.xdk file for consistency
    - updated <project-name>.xdk file contents to 3.0.0 spec
    - updated and/or added .gitignore that reflects project archive needs
    - updated app.json to reflect need for "runtime", "targets" and "keywords"
    - cleaned up app.json to remove unused properties
    - updated README.md for consistency with other samples
    - updated and/or added LICENSE.md for consistency and completeness

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-/*jslint node:true, vars:true, bitwise:true, unparam:true */
-/*jshint unused:true */
-/*global */
 /*
-A simple node.js application intended to read data from Analog pins on the Intel based development boards 
-such as the Intel(R) Galileo and Edison with Arduino breakout board, and display it in a browser running on the client.
+ * Intel XDK IoT Node.js Web Server app.
+ *
+ * Read data from analog pins on an Intel IoT development board
+ * and display the results via a browser running on a client device.
+ * This app demonstrates the use of http.createServer and fs.
+ *
+ * This sample has only been tested on a Galileo and Edison board. It may run on
+ * other IoT Node.js platforms, but may require changes to the I/O initialization
+ * and configuration code.
+ *
+ * https://software.intel.com/en-us/xdk/docs/lp-xdk-iot
+ * https://software.intel.com/en-us/xdk-sample-creating-a-web-server
+ */
 
-This demonstrates use of http.createServer, and fs.
 
-MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
-Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
-  
-Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
-Using a ssh client: 
-1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
-2. opkg update
-3. opkg upgrade
+// keep these lines (below) for proper jshinting and jslinting
+/*jslint node:true, vars:true, bitwise:true */
+/*jshint unused:true, undef:true */
+// see http://www.jslint.com/help.html and http://jshint.com/docs
 
-Article: https://software.intel.com/en-us/xdk-sample-creating-a-web-server
-*/
 
-// Set this to the ip address of your board (not 127.0.0.1)
-var ipAddress = '192.168.2.15'; 
+var ipAddress = '192.168.2.15' ;    // Set to IP address of your board (not 127.0.0.1)
 
-var mraa = require('mraa'); //require mraa
-console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+var mraa = require("mraa") ;        // get mraa objecat and write version to the console
+console.log('MRAA Version: ' + mraa.getVersion()) ;
 
-// Start by loading in some data
-var fs = require('fs');
-
-var lightSensorPage = fs.readFileSync('/node_app_slot/lightsensor.html');
+var fs = require('fs') ;            // start by loading in some data
+var lightSensorPage = fs.readFileSync('/node_app_slot/index.html') ;
 
 // Insert the ip address in the code in the page
-
 lightSensorPage = String(lightSensorPage).replace(/<<ipAddress>>/, ipAddress);
 
 var analogPin0 = new mraa.Aio(0);
@@ -39,7 +36,7 @@ var analogPin0 = new mraa.Aio(0);
 /**
  * Given a value, convert it to Lux
  *
- * This uses the table given in the documentation for the 
+ * This uses the table given in the documentation for the
  * Grove Starter Kit Plus. We have not sought to verify these
  * values with our device. That would be worth doing if you
  * intend to rely on these values. In that case, it could also
@@ -66,7 +63,7 @@ function getLux(analogValue) {
   if (i > 0) {
     i = i - 1;
   }
-  // simple linear interpolation 
+  // simple linear interpolation
   lux =  (calib[i].lux *(calib[i + 1].reading - analogValue) + calib[i + 1].lux * (analogValue - calib[i].reading))/(calib[i + 1].reading - calib[i].reading);
   return lux;
 }
@@ -76,7 +73,7 @@ http.createServer(function (req, res) {
     var value;
     // This is a very quick and dirty way of detecting a request for the page
     // versus a request for light values
-    if (req.url.indexOf('lightsensor') != -1) {
+    if (req.url.indexOf('index') != -1) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(lightSensorPage);
     }
